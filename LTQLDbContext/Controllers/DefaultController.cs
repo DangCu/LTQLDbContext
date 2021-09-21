0b878bab4fb66363b7c126c0d940183049016d2d
cdang5969@gmail.com <cdang5969@gmail.com>
2021-09-21 17:28:46 +0700

    .

---
+++ LTQLDbContext/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LTQLDbContext.Models;
+
+namespace LTQLDbContext.Controllers
+{
+    public class DefaultController : Controller
+    {
+        
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
